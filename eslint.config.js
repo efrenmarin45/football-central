fa28657645535c05fd8be49e2301a10abb4d246b
Efren Marin <efren45marin@gmail.com>
2025-06-09 19:08:16 -0700

    Updated config file

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,23 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react-x'
+import reactDom from "eslint-plugin-react-dom";
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended,
+    tseslint.configs.recommended,
+    react.configs.recommended, reactDom.configs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
@@ -23,6 +33,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "react-x/no-class-component": "warn",
+      "react-dom/no-dangerously-set-innerhtml": "warn",
     },
   },
 )
